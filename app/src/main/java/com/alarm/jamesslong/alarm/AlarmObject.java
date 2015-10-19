ab7aad0fa74a094a1ca2215f8f8bc1ba4bdae7fd
James Slong <jamesslong@Jamess-iMac.hq.mirthcorp.com>
2015-10-19 16:51:21 -0700

    Push 1

---
+++ app/src/main/java/com/alarm/jamesslong/alarm/AlarmObject.java
@@ -0,0 +1,7 @@
+package com.alarm.jamesslong.alarm;
+
+/**
+ * Created by jamesslong on 10/19/15.
+ */
+public class AlarmObject {
+}
--- app/src/main/java/com/alarm/jamesslong/alarm/Alarm_Main.java
+++ app/src/main/java/com/alarm/jamesslong/alarm/Alarm_Main.java
@@ -1,28 +1,47 @@
 package com.alarm.jamesslong.alarm;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class Alarm_Main extends Activity {
+    ListView listView;
+//    AlarmAdapter adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_main);
-        Button addAlarm = (Button) findViewById(R.id.addAlarm);
-        addAlarm.setOnClickListener(new View.OnClickListener() {
+        listView = (ListView) findViewById(R.id.alarmList);
+
+        FloatingActionButton addAlarmButton = (FloatingActionButton) findViewById(R.id.addAlarm);
+        addAlarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Add_Alarm.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                //TODO
+                Toast.makeText(getApplicationContext(), "Clicked Button", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+
+//    public class AlarmAdapter extends ArrayAdapter<AlarmObject>{
+//        public MedicationsAdapter(Context context, ArrayList<AlarmObject> alarmList){
+//            super(context, 0, alarmList);
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent){
+//            AlarmObject alarmObject = getItem(position);
+//            if(convertView == null){
+//                convertView = LayoutInflater.from(getContext()).inflate()
+//            }
+//        }
+//    }
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
